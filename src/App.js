3ebf27d5107cd3f1831fa80781a368c1718f3e85
rafi365 <muhammad.rafi3@student.umn.ac.id>
2020-12-07 18:41:49 +0700

    timer with set time logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+var test = 10;
+  function add(){
+    test = test + 1;
+    alert(test)
+  };
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {hour: 0,minute: 0,second: 0};
+  }
+  increase_timer(x){
+    if (x == 1 && this.state.hour < 59) {
+      this.setState({hour: this.state.hour + 1});
+      
+    }
+    else if (x == 2 && this.state.minute < 59) {
+      this.setState({minute: this.state.minute + 1});
+      
+    }
+    else if (x == 3 && this.state.second < 59) {
+      this.setState({second: this.state.second + 1});
+      
+    }
+  }
+  decrease_timer(x){
+    if (x == 1 && this.state.hour > 0) {
+      this.setState({hour: this.state.hour - 1});
+      
+    }
+    else if (x == 2 && this.state.minute > 0) {
+      this.setState({minute: this.state.minute - 1});
+      
+    }
+    else if (x == 3 && this.state.second > 0) {
+      this.setState({second: this.state.second - 1});
+      
+    }
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>Simple timer</h1>
+        <div className="sideways">
+        <div>
+          {/* hour */}
+          <button onClick={()=>{this.increase_timer(1)}}>up</button>
+          <h3>{this.state.hour.toString().padStart(2,'0')}</h3>
+          <button onClick={()=>{this.decrease_timer(1)}}>down</button>
+        </div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <div>
+        
+          {/* minutes */}
+          <button onClick={()=>{this.increase_timer(2)}}>up</button>
+          <h3>{this.state.minute.toString().padStart(2,'0')}</h3>
+          <button onClick={()=>{this.decrease_timer(2)}}>down</button>
+        </div>
+
+        <div>
+          {/* seconds */}
+          <button onClick={()=>{this.increase_timer(3)}}>up</button>
+          <h3>{this.state.second.toString().padStart(2,'0')}</h3>
+          <button onClick={()=>{this.decrease_timer(3)}}>down</button>
+        </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
